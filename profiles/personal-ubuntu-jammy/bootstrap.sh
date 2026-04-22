76eea1711f596bc31a5ce0de5bd44333d71adad2
srt3ch <sryan@srt3ch.com>
2026-04-22 12:19:19 -0400

    Add --fix-missing to apt install to handle transient download failures

--- profiles/personal-ubuntu-jammy/bootstrap.sh
+++ profiles/personal-ubuntu-jammy/bootstrap.sh
@@ -50,7 +50,7 @@ echo "[2/7] Updating package lists..."
 apt-get update -q
 
 echo "[3/7] Installing packages..."
-apt-get install -y \
+apt-get install -y --fix-missing \
   linux-headers-$(uname -r) \
   virtualbox-guest-utils \
   virtualbox-guest-x11 \
