eaac3745e1e22a1aade7fb2512a3f18d19fd0342
srt3ch <sryan@srt3ch.com>
2026-04-22 15:01:06 -0400

    Use ~ instead of hardcoded home path in SSH aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,4 +1,4 @@
-alias connectdev='ssh -i /home/user/.ssh/default dev@10.1.4.23'
-alias connecthost='ssh -i /home/user/.ssh/default dev@10.1.2.25'
+alias connectdev='ssh -i ~/.ssh/default dev@10.1.4.23'
+alias connecthost='ssh -i ~/.ssh/default dev@10.1.2.25'
 alias fullupdate='sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y'
 alias hdspace='df -h'
